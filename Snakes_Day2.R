dac55512a2ff8531198f8e53d675c24e057d84ef
MpumalangaMnyekemfu <4175370@myuwc.ac.za>
2021-04-21 03:12:37 +0200

    Snakes_Day2Mpumalanga

---
+++ Snakes_Day2.R
@@ -0,0 +1,38 @@
+snakes <- read_csv("data/snakes.csv")
+snakes$day = as.factor(snakes$day)
+
+snakes.summary <- snakes %>% 
+  group_by(day, snake) %>% 
+  summarise(mean_openings = mean(openings),
+            sd_openings = sd(openings)) %>% 
+  ungroup()
+snakes.summary
+
+snakes.summary <- snakes %>% 
+  group_by(day) %>% 
+  summarise(mean_openings = mean(openings),
+            sd_openings = sd(openings)) %>% 
+  ungroup()
+snakes.summary
+
+library(Rmisc)
+snakes.summary2 <- summarySE(data = snakes, measurevar = "openings", groupvars = c("day"))
+
+ggplot(data = snakes, aes(x = day, y = openings)) +
+  geom_segment(data = snakes.summary2, aes(x = day, xend = day, y = openings - ci, yend = openings + ci, colour = day),
+               size = 2.0, linetype = "solid", show.legend = F) +
+  geom_boxplot(aes(fill = day), alpha = 0.6, show.legend = F) + 
+  geom_jitter(width = 0.05)
+
+snakes.aov <- aov(openings ~ day + snake, data = snakes)
+summary(snakes.aov)
+
+par(mfrow = c(2, 2))
+
+snakes.res <- residuals(snakes.aov)
+hist(snakes.res, col = "purple")
+
+plot(fitted(snakes.aov), residuals(snakes.aov), col = "green")
+
+snakes.tukey <- TukeyHSD(snakes.aov, which = "day", conf.level = 0.90)
+plot(snakes.tukey, las = 1, col = "brown")
